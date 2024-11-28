9212b8516c176d4ce03163eaa4720ecb4d9b92e4
Daniel Medina <daniel@medina.com>
2024-11-28 17:34:01 -0600

    :white_check_mark: Updated tests

--- test.cpp
+++ test.cpp
@@ -46,50 +46,6 @@ void testAddSupplier() {
     }
 }
 
-void testRemoveProduct() {
-    Inventory inventory = Inventory();
-    Supplier supplier("Test Supplier", "123456789", "Test Address", 1);
-    Category category("Test Category", "Test Description", 1);
-    Product product("Test Product", 10.0, 100, &supplier, &category, 1);
-
-    inventory.addProduct(&product);
-    inventory.removeProduct(1);
-
-    if (inventory.getAllProducts().size() == 0) {
-        std::cout << "✅ testRemoveProduct passed." << std::endl;
-    } else {
-        std::cout << "❌ testRemoveProduct failed." << std::endl;
-    }
-}
-
-void testRemoveCategory() {
-    Inventory inventory = Inventory();
-    Category category("Test Category", "Test Description", 1);
-
-    inventory.addCategory(&category);
-    inventory.removeCategory(1);
-
-    if (inventory.getAllCategories().size() == 0) {
-        std::cout << "✅ testRemoveCategory passed." << std::endl;
-    } else {
-        std::cout << "❌ testRemoveCategory failed." << std::endl;
-    }
-}
-
-void testRemoveSupplier() {
-    Inventory inventory = Inventory();
-    Supplier supplier("Test Supplier", "123456789", "Test Address", 1);
-
-    inventory.addSupplier(&supplier);
-    inventory.removeSupplier(1);
-
-    if (inventory.getAllSuppliers().size() == 0) {
-        std::cout << "✅ testRemoveSupplier passed." << std::endl;
-    } else {
-        std::cout << "❌ testRemoveSupplier failed." << std::endl;
-    }
-}
-
 int main() {
     testAddProduct();
     testAddCategory();
